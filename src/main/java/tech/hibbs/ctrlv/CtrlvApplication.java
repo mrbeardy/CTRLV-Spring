03f21c5aeac47fc95e8f53b7f4051dfd2caa3b1f
Michael Hibbs <hibbs.michael@gmail.com>
2022-10-23 00:29:45 +0100

    Initial commit with home screen and basic layout

---
+++ src/main/java/tech/hibbs/ctrlv/CtrlvApplication.java
@@ -0,0 +1,13 @@
+package tech.hibbs.ctrlv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/hibbs/ctrlv/CtrlvApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hibbs.ctrlv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
